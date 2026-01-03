47fc77c92c8e8c8a3854489e2a704eb91eaffd7f
cloyucsd <122492228+cloyucsd@users.noreply.github.com>
2026-01-02 16:23:57 -0800

    Add project entries

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -11,7 +11,18 @@ let render_projects = (slug) => {
     // sort by recency, include course info, truncate list by default
     let projects_obj = [
         {
-            image: 'assets/images/mentors.jpg',
+            image: 'assets/images/wikitext.png',
+            link: 'https://github.com/cieloaloy/wikipedia-text-categorization',
+            title: 'Wikipedia Text Categorization',
+            date: "Summer 2025",
+            categories: ['featured'],
+            demo: 'https://github.com/cieloaloy/wikipedia-text-categorization',
+            technologies: ['Python', 'Machine Learning'],
+            description: "A machine learning experiment that classifies Wikipedia articles using NLP across a Naive Bayes model and SVM.",
+            course: "CSE 150A at UC San Diego"
+        },
+        {
+            image: 'assets/images/1111.png',
             link: 'https://github.com/cse110-sp24-group11/cse110-sp24-group11',
             title: '11:11 Dev Journal',
             date: "Fall 2023",
@@ -20,6 +31,17 @@ let render_projects = (slug) => {
             technologies: ['Full-Stack', 'E2E Testing'],
             description: "A built-from-scratch CRUD journaling web app for developers. Uses a CI/CD pipeline via GitHub Actions and a full testing suite via Jest and Puppeteer.",
             course: "CSE 110 at UC San Diego"
+        },
+        {
+            image: 'assets/images/15lab.png',
+            link: 'https://cieloaloy.github.io/cse15l-lab-reports/',
+            title: 'Software Techniques Labs',
+            date: "Spring 2023",
+            categories: ['featured'],
+            demo: 'https://cieloaloy.github.io/cse15l-lab-reports/',
+            technologies: ['Linux', 'Git', 'Terminal Commands'],
+            description: "A series of 5 lab reports covering essential software techniques including Git, Linux, and helpful terminal commands.",
+            course: "CSE 15L at UC San Diego"
         },
     ]
 
@@ -41,7 +63,7 @@ let project_mapper = project => {
 
                 ${project.image ? 
                     `<div class="card__image border-tlr-radius">
-                        <a href="${project.link}">
+                        <a href="${project.demo}">
                             <img src="${project.image}" alt="image" id="project-image" class="border-tlr-radius">
                         </a>
                     </div>`           
@@ -55,7 +77,7 @@ let project_mapper = project => {
                             ${project.date ? `<span class="post-date">${project.date}</span>` : ''}
                         </h2>
 
-                        <p class="paragraph-text-normal">${project.description} ${project.demo ? `<a href="${project.demo}">Demo</a>` : ''}</p>
+                        <p class="paragraph-text-normal">${project.description}</p>
                     </article>
 
                                 
